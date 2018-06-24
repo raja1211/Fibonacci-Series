9ad6d67d95eab512c2a2fc2bf32572b53c1492e9
Nataraj <natarajasekhar.b@gmail.com>
2018-06-24 12:59:06 +0000

    [Flask_api] Realligned Code

--- salt/Flask/fibnocci/flask_api.py
+++ salt/Flask/fibnocci/flask_api.py
@@ -11,31 +11,30 @@ def index():
 
 @app.route('/fib/<string:value>',methods=['GET'])
 def fib(value):
- try:
-   n = int(value)
-   if n < 0:
-   	return 'Sorry you entered negative number \n'
- except:
-   return 'Seems to be you enterd unsupported value\n'
+ 	ans_list = []
+ 	global global_cache
+
+	try:
+   	   n = int(value)
+   	   if n < 0:
+   	      return 'negative number - wrong input'
+ 	except:
+              return 'wrong input'
    
- ans_list = []
- global global_cache
- if n in global_cache.keys():
-     ans_list = str(global_cache[n])
- elif n < 1:
-     ans = 0 
-     global_cache[n] = ans
-#     ans_list = ans
- else:
-     a,b = 0,1
-     for i in range(n):
-         ans_list.append(a)
-         a,b = b,a+b
-     global_cache[n] = ans_list
- return str(ans_list)
+ 	if n in global_cache.keys():
+ 	    	ans_list = str(global_cache[n])
+ 	elif n < 1:
+     		ans = 0 
+     		global_cache[n] = ans
+	else:
+     		a,b = 0,1
+     		for i in range(n):
+         		ans_list.append(a)
+         		a,b = b,a+b
+     		global_cache[n] = ans_list
 
-
+ 	return str(ans_list)
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+	app.run(debug=True)
